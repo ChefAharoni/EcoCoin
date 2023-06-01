df28f847f2a777468ccf68946ade39e463c053dc
Star Chef <128301@mail.lsue.edu>
2023-06-01 13:58:26 +0300

    Added files from mini-blockchain course on `codecademy`. Fixed a type in index.html Added a flask app file from the book.

---
+++ 01_Studying_Ethereum/II_Implementing_Blockchain/flask_course/tmp/book_app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello world!</h1>'
+
+
+@app.route('/user/<name>')
+def name():
+    return f'Hello {name}! Welcome to our beautiful website!'
+
+
+if __name__ == "__main__":
+    app.run()
