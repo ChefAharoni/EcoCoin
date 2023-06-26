d38627725bc762fa4cd4605170efce417f4c2edb
Star Chef <128301@mail.lsue.edu>
2023-06-26 19:43:56 +0300

    Updating and fixing packages

--- foundry_ws/test/TestWorkflow.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8;
-
-import {Test} from "../lib/forge-std/src/test.sol";
-
-contract TestWorkflow is Test {
-    function testWorkflow() public {}
-}
