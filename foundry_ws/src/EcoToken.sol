537df4c6f349ed12c42888610d39a2df86037cd5
Star Chef <128301@mail.lsue.edu>
2023-06-26 19:04:14 +0300

    Packages updates and fixes

--- foundry_ws/src/EcoToken.sol
+++ foundry_ws/src/EcoToken.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-// import "foundry_ws/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 // import "./openzeppelin-contracts/contracts/access/Ownable.sol"; // Doesn't work for some reason, implement in the future.
 // foundry_ws/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol
