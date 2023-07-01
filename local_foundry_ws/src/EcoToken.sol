318f1769e9e13148cc480b298bbd5375a005a830
Star Chef <128301@mail.lsue.edu>
2023-07-01 19:44:12 +0100

    Updated Municipality & EcoToken
    Added library for implementing the municipality struct, and a contract within the same file to store the data of the municipality.
    
    Main error as of now is the option to mint and burn tokens wihtout fully inherting on instancing the EcoToken,  since it requires the contstructor variables to be set each time.

--- local_foundry_ws/src/EcoToken.sol
+++ local_foundry_ws/src/EcoToken.sol
@@ -24,10 +24,11 @@
 pragma solidity ^0.8;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Muni, MuniData} from "./Municipality.sol";
 
 // import "./openzeppelin-contracts/contracts/access/Ownable.sol"; // Doesn't work for some reason, implement in the future.
 
-error EcoCoin__NotMunicipality(string errorMsg); // Error to throw when the caller is not the i_tokenOwner.
+// error EcoCoin__NotMunicipality(string errorMsg); // Error to throw when the caller is not the i_tokenOwner.
 
 // TODO - add more error and revert messages instead of require, to save gas.
 // TODO - add s_ prefix to variables saved in storage.
@@ -44,19 +45,20 @@ error EcoCoin__NotMunicipality(string errorMsg); // Error to throw when the call
  * @notice  .
  */
 
-contract EcoCoin is ERC20 {
+contract EcoCoin is ERC20, MuniData {
+    using Muni for *;
+
     // To set the i_tokenOwner of the token; used for managing roles.
     // address immutable i_tokenOwner = msg.sender; // i_ prefix means immutable.
-    Municipality private i_genMunicipality; // Genesis municipality, will be able to assign other municipalities and assign roles; should be immutable
-    string private constant NOT_MUNICIPALITY_MSG =
-        "Only a municipality can perform this action!"; // Error message to throw when the caller is not the i_tokenOwner. // Seems it's not common to use strings in custom errors - I'll keep it here for now.
+    Muni.Municipality private i_genMunicipality; // Genesis municipality, will be able to assign other municipalities and assign roles; should be immutable
 
-    mapping(address => string) public MuniAddrToZipCode; // Mapping of address to a municipality zip code.
-    mapping(address => Municipality) public municipalities; // Mapping of all municipalites of type Municipality; used to check if a msg.sender is of type Municipality.
-    struct Municipality {
-        address muniAddr; // Address of the Municipality
-        string s_muniZipCode; // Zip code location of the genesis municipality; string so it can handle long zip codes and non-us zipcodes as well.
-    }
+    // string private constant NOT_MUNICIPALITY_MSG =
+    //     "Only a municipality can perform this action!"; // Error message to throw when the caller is not the i_tokenOwner. // Seems it's not common to use strings in custom errors - I'll keep it here for now.
+
+    // struct Municipality {
+    //     address muniAddr; // Address of the Municipality
+    //     string s_muniZipCode; // Zip code location of the genesis municipality; string so it can handle long zip codes and non-us zipcodes as well.
+    // }
 
     constructor(
         address _genMunicipalityAddr,
@@ -68,39 +70,15 @@ contract EcoCoin is ERC20 {
         // Update - do not mint any tokens at start.
         // uint256 n = 1000; // Number of tokens to create to the contract deployer.
         // _mint(msg.sender, n * 10 ** uint(decimals())); // Decimals function return 18 == 18 decimal places; here I changed it to 0 so there won't be any decimals. //! mint opeation was cancelled - will mint only when bottles are deposited.
-        i_genMunicipality = Municipality(
+        i_genMunicipality = Muni.Municipality(
             _genMunicipalityAddr,
             _genMunicipalityZipCode
         );
-        addMuni({
-            _municipalityAddr: _genMunicipalityAddr,
-            _municipalityZipCode: _genMunicipalityZipCode
+        // Because the function addMuni requires msg.sender to be a municipality, implementing the function here so it could be called once automatically without restrictions.
+        municipalities[_genMunicipalityAddr] = Muni.Municipality({
+            muniAddr: _genMunicipalityAddr,
+            s_muniZipCode: _genMunicipalityZipCode
         });
-    }
-
-    /**
-     * @notice  Addition to function so only the i_tokenOwner can perform actions.
-     * @dev     Not sure this method works with functions that are called from other contracts.
-     */
-    modifier muniOnly() {
-        if (municipalities[msg.sender].muniAddr != msg.sender) {
-            /* Since all keys in mapping are set to address(0) by default, checks if the address exists in the mapping. */
-            revert EcoCoin__NotMunicipality(NOT_MUNICIPALITY_MSG);
-        }
-        _;
-    }
-
-    function addMuni(
-        address _municipalityAddr,
-        string memory _municipalityZipCode
-    ) public returns (Municipality memory) {
-        // Function to add a municipality to the mapping.
-        MuniAddrToZipCode[_municipalityAddr] = _municipalityZipCode;
-        municipalities[_municipalityAddr] = Municipality({
-            muniAddr: _municipalityAddr,
-            s_muniZipCode: _municipalityZipCode
-        });
-        return municipalities[_municipalityAddr];
     }
 
     /**
@@ -113,15 +91,15 @@ contract EcoCoin is ERC20 {
     //     return i_tokenOwner;
     // }
 
-    // /**
-    //  * @notice  Function that mints tokens to the i_tokenOwner.
-    //  * @param   n  Amount of tokens to mint.
-    //  */
-    // function mintTokens(uint256 n) public {
-    //     // ERC20 tokens by default have 18 decimals
-    //     // number of tokens minted = n * 10^18
-    //     _mint(i_tokenOwner, n * 10 ** uint(decimals())); // Decimals function return 18 == 18 decimal places
-    // }
+    /**
+     * @notice  Function that mints tokens to the machine.
+     * @param   n  Amount of tokens to mint.
+     */
+    function mintTokens(address machineAdr,uint256 n) public muniOnly {
+        // ERC20 tokens by default have 18 decimals
+        // number of tokens minted = n * 10^18
+        _mint(machineAdr, n * 10 ** uint(decimals())); // Decimals function return 18 == 18 decimal places
+    }
 
     /**
      * @notice  Function to transfer tokens from one address to another.
@@ -132,32 +110,30 @@ contract EcoCoin is ERC20 {
      * @return  bool  True if the transfer was successful.
      */
     //! the _transfer function can be inherited from ERC20, and the sender would be the machine, who would also be the tokens minter.
-    // function transferFunds(
-    //     // Double check if I need this function, I may be able to inherit it from this contract.
-    //     address sender,
-    //     address recipient,
-    //     uint amount
-    // ) public returns (bool) {
-    //     _transfer(sender, payable(recipient), amount); // payable keyword means that the receipent can accept eth (or tokens).
-    //     return true;
-    // }
+    function transferFunds(
+        // Double check if I need this function, I may be able to inherit it from this contract.
+        address sender,
+        address recipient,
+        uint amount
+    ) public muniOnly returns (bool) {
+        _transfer(sender, payable(recipient), amount); // payable keyword means that the receipent can accept eth (or tokens).
+        return true;
+    }
 
-    // /**
-    //  * @notice  Function to burn tokens.
-    //  * @dev     Uses the _burn function from the ERC20 contract, because it's internal.
-    //  * @param   account  Account to burn tokens from.
-    //  * @param   amount  Amount of tokens to burn.
-    //  * @return  bool  True if the burn was successful.
-    //  */
-    // function _burnTokens(
-    //     address account,
-    //     uint256 amount
-    // ) public muniOnly returns (bool) {
-    //     //---!!!--- I think the muniOnly gives problems when this function is called from outside - it throws an error even though the caller is the owner. ---!!!---
-
-    //     _burn(account, amount); // Burn tokens
-    //     return true;
-    // }
+    /**
+     * @notice  Function to burn tokens.
+     * @dev     Uses the _burn function from the ERC20 contract, because it's internal.
+     * @param   account  Account to burn tokens from.
+     * @param   amount  Amount of tokens to burn.
+     * @return  bool  True if the burn was successful.
+     */
+    function _burnTokens(
+        address account,
+        uint256 amount
+    ) public muniOnly returns (bool) {
+        _burn(account, amount); // Burn tokens
+        return true;
+    }
 
     /**
      * @notice  Overriding the decimals function from the ERC20 contract, setting the decimals of the token to zero (0).
---
+++ local_foundry_ws/src/Municipality.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+import {EcoCoin} from "./EcoToken.sol";
+
+error MuniData__NotMunicipality(string errorMsg); // Error to throw when the caller is not the i_tokenOwner.
+
+library Muni {
+    struct Municipality {
+        address muniAddr; // Address of the Municipality
+        string s_muniZipCode; // Zip code location of the genesis municipality; string so it can handle long zip codes and non-us zipcodes as well.
+    }
+
+    /**
+     * @notice  Adds a municipality to the mappings.
+     * @dev     Should be called only by another municipality using a modifier muniOnly() from MuniData contract.
+     * @dev This function should be called ON the municipalities mapping.
+     * @dev i.e. municipalities.addMuni(_municipalityAddr, _municipalityZipCode) muniOnly().
+     * @param  municipalities  Mapping of all municipalites of type Municipality.
+     * @param _municipalityAddr  Address of the municipality to add.
+     * @param _municipalityZipCode  Zip code of the municipality to add.
+     * @return  address   Address of the municipality added.
+     */
+    function addMuni(
+        mapping(address => Municipality) storage municipalities,
+        address _municipalityAddr,
+        string memory _municipalityZipCode
+    ) public returns (Muni.Municipality memory) {
+        // Function to add a municipality to the mapping.
+        // MuniAddrToZipCode[_municipalityAddr] = _municipalityZipCode;
+        municipalities[_municipalityAddr] = Muni.Municipality({
+            muniAddr: _municipalityAddr,
+            s_muniZipCode: _municipalityZipCode
+        });
+        return municipalities[_municipalityAddr];
+    }
+}
+
+contract MuniData {
+    // EcoCoin ecoCoin = new EcoCoin;
+    using Muni for *;
+    mapping(address => Muni.Municipality) municipalities; // Mapping of all municipalites of type Municipality; used to check if a msg.sender is of type Municipality.
+    mapping(address => string) public MuniAddrToZipCode; // Mapping of address to a municipality zip code.
+    string private constant NOT_MUNICIPALITY_MSG =
+        "Only a municipality can perform this action!"; // Error message to throw when the caller is not the i_tokenOwner. // Seems it's not common to use strings in custom errors - I'll keep it here for now.
+
+    /**
+     * @notice  Addition to functions so only municipalities can perform actions.
+     * @dev   .
+     */
+    modifier muniOnly() {
+        if (municipalities[msg.sender].muniAddr != msg.sender) {
+            /* Since all keys in mapping are set to address(0) by default, checks if the address exists in the mapping. */
+            revert MuniData__NotMunicipality(NOT_MUNICIPALITY_MSG);
+        }
+        _;
+    }
+}
